73bebca0579b5a95ae8d1ba7e6611479cc713720
Khodiar Bhagwan <Khodiarbhagwan@gmail.com>
2021-04-02 00:56:07 +0530

    rendering templates in differnet ways

--- render_template.py
+++ render_template.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 @app.route('/hello/<user>')
 def index(user):
    return render_template('hello.html', name=user)
+   
+
+@app.route('/hello/<int:score>')
+def hello_name(score):
+   return render_template('marks.html', marks = score)
+   
+   
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
